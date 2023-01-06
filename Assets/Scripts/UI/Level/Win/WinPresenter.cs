9cd373396a2c0730512c2cecb85ac17f6dc97675
dimdimich123 <frostm333@gmail.com>
2023-01-06 21:19:23 +0700

    Created Lose and Win UI

---
+++ Assets/Scripts/UI/Level/Win/WinPresenter.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace UI.Level.Win
+{
+    [RequireComponent(typeof(WinView))]
+    public sealed class WinPresenter : MonoBehaviour
+    {
+        private WinView _view;
+        private WinModel _model;
+
+        private void Awake()
+        {
+            _view = GetComponent<WinView>();
+            _model = new WinModel();
+        }
+
+        private void OnEnable()
+        {
+            _view.OnExit += Exit;
+            _view.OnRestart += Restart;
+        }
+
+        private void Exit()
+        {
+            _model.Exit();
+        }
+
+        private void Restart()
+        {
+            _model.Restart();
+        }
+
+        private void OnDisable()
+        {
+            _view.OnExit -= Exit;
+            _view.OnRestart -= Restart;
+        }
+    }
+}
